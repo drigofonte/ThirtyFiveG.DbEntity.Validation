30a971302cc310f3a8a1faf83cceb7eb0a3a8d4e
Rodrigo F. Carvalho <rodrigo@35geeks.com>
2018-12-28 12:35:17 +0000

    Updated project description

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ThirtyFiveG.DbEntity.Validation")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Project to support the validation of DbEntity objects")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("30 Five Geeks")]
 [assembly: AssemblyProduct("ThirtyFiveG.DbEntity.Validation")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
